03c32cc013a9a6670574a04a229903932a154b20
mborreros <mlbsindia@gmail.com>
2022-02-04 11:40:24 -0500

    commit tree bdbab6acfc5235f6666d45ec11c62888085d4770

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,19 @@ require 'pry'
 class String
 
   def sentence?
-    
+    # check if array contains "." character
+    self.include? "."
   end
 
   def question?
-
+    self.include? "?" 
   end
 
   def exclamation?
-
+    self.include? "!" 
   end
 
   def count_sentences
-
+    self.split(/[[!?.]]\s/).count
   end
 end
